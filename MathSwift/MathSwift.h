1cccc9ccb57dc059fc879c47a39b3712fedac368
ENYAN HUANG <huangenyan@gmail.com>
2014-10-10 16:45:38 +0800

    Initial commit

---
+++ MathSwift/MathSwift.h
@@ -0,0 +1,19 @@
+//
+//  MathSwift.h
+//  MathSwift
+//
+//  Created by Enyan Huang on 10/10/14.
+//  Copyright (c) 2014 The Hong Kong Polytechnic University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MathSwift.
+FOUNDATION_EXPORT double MathSwiftVersionNumber;
+
+//! Project version string for MathSwift.
+FOUNDATION_EXPORT const unsigned char MathSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MathSwift/PublicHeader.h>
+
+
