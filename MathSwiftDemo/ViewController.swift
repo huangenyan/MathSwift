38239dcd813c6e2ae70e67845b96b41707707adf
Evan E.Y. Huang <huangenyan@gmail.com>
2016-10-26 16:36:32 +0800

    To add a demo

---
+++ MathSwiftDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MathSwiftDemo
+//
+//  Created by Enyan HUANG on 26/10/2016.
+//
+//
+
+import UIKit
+import MathSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
